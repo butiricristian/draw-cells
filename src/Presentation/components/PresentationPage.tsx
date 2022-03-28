482d483836c4632b634007f05b635b5460b887b6
Cristian Butiri <butiri.cristian@gmail.com>
2022-03-28 19:51:15 +0300

    fix lint issue

--- src/Presentation/components/PresentationPage.tsx
+++ src/Presentation/components/PresentationPage.tsx
@@ -1,14 +1,12 @@
 import { child, get, ref } from 'firebase/database';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 import { db } from '../../firebase-config';
 import State from '../../stateInterface';
 import PresentationContainer from './PresentationContainer';
 
 
 const PresentationPage = () => {
-  const { presentationId } = useParams()
   const user = useSelector((state: State) => state.home.user)
 
   useEffect(() => {
